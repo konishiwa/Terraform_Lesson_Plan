385648c4929a37d9a048547d87fc6712ebb7dbae
Haruka Konishi <konishiharuka@gmail.com>
2023-10-30 00:44:33 -0400

    run terraform fmt

--- lessons/lesson5/main.tf
+++ lessons/lesson5/main.tf
@@ -1,12 +1,19 @@
+//Example Provider: AWS
+provider "aws" {
+  region  = "use-east-1" //configuration parameters
+  version = "3.7.0"
+}
+
+
 resource "aws_security_group" "example-sg" {
   name = "example-security-group"
-  vpc = aws_vpc.my-vpc.id
+  vpc  = aws_vpc.my-vpc.id
   dynamic "ingress" {
     for_each = var.rules //iterates over following, so you don't have to create 20 ingress rules
     content {
-      from_port = ingress.value["port"]
-      to_port = ingress.value["port"]
-      protocol = ingress.value["proto"]
+      from_port   = ingress.value["port"]
+      to_port     = ingress.value["port"]
+      protocol    = ingress.value["proto"]
       cidr_blocks = ingress.value["cidrs"]
     }
   }
--- lessons/lesson5/variables.tf
+++ lessons/lesson5/variables.tf
@@ -1,13 +1,13 @@
 variable "rules" {
   default = [
     {
-      port = 80
-      proto = "tcp"
+      port        = 80
+      proto       = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     },
     {
-      port = 22
-      proto = "tcp"
+      port        = 22
+      proto       = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   ]
